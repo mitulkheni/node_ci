58805c882b0754a053a7adda41370d9d28ccb40c
mitulkheni <mitulkheni695@gmail.com>
2022-11-24 13:37:29 +0530

    Travisconfig

---
+++ services/cache.js
@@ -0,0 +1,45 @@
+const mongoose = require("mongoose");
+const redis = require("redis");
+const util = require("util");
+const keys = require("../config/keys");
+
+const client = redis.createClient(keys.redisUrl);
+client.hget = util.promisify(client.hget);
+const exec = mongoose.Query.prototype.exec;
+
+mongoose.Query.prototype.cache = function (options = {}) {
+  this.useCache = true;
+  this.hashKey = JSON.stringify(options.key || "");
+  return this;
+};
+
+mongoose.Query.prototype.exec = async function () {
+  if (!this.useCache) {
+    return exec.apply(this, arguments);
+  }
+
+  const key = JSON.stringify(
+    Object.assign({}, this.getQuery(), {
+      collection: this.mongooseCollection.name,
+    })
+  );
+
+  const cacheValue = await client.hget(this.hashKey, key);
+
+  if (cacheValue) {
+    const docs = JSON.parse(cacheValue);
+    return Array.isArray(docs)
+      ? docs.map((doc) => new this.model(doc))
+      : new this.model(docs);
+  }
+
+  const result = await exec.apply(this, arguments);
+  client.hmset(this.hashKey, key, JSON.stringify(result), "EX", 10);
+  return result;
+};
+
+module.exports = {
+  clearHash(hashKey) {
+    client.del(JSON.stringify(hashKey));
+  },
+};
